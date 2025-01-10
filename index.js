9b5621e89a378490b4ff5f69d5dc9e4096378d52
AImightyFinest <yacinebelabes93@gmail.com>
2025-01-10 08:49:07 +0100

    renamed this repo

---
+++ index.js
@@ -0,0 +1,40 @@
+console.log ("Calculator")
+// CALCULATOR PROGRAM
+
+
+
+const display = document.getElementById("display");
+
+
+
+function appendToDisplay(input){
+
+    display.value += input;
+
+}
+
+
+
+function clearDisplay(){
+
+    display.value = "";
+
+}
+
+
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value);
+
+    }
+
+    catch(error){
+
+        display.value = "Error";
+
+    }
+
+}
